0f5bf918eb2c108c1187b4725ff391215fd61b40
lizeyang <lizeyang@yy.com>
2016-01-05 17:30:07 +0800

    可以新增todo项

--- pages/todo/todo.ctrl.js
+++ pages/todo/todo.ctrl.js
@@ -1,17 +1,25 @@
-angular.module('myApp').controller('TodoCtrl', function($scope){
+angular.module('myApp').controller('TodoCtrl', function ($scope) {
 
     $scope.list = getToDoList();
-
+    $scope.newTodo = "";
 
     //$scope.name = 'lizeyang';
 
+    $scope.addTodo = function () {
+        $scope.list.push({createTime: new Date().getTime(), text: $scope.newTodo});
+    }
+
+    $scope.isShowInputTip = function(){
+        return $scope.newTodo && $scope.newTodo.length != 0;
+    }
+
 });
 
-function getToDoList(){
+function getToDoList() {
     return [
-        "准备周三分享",
-        "Q4绩效考核",
-        "消息中心开发",
-        "公共组件交互"
+        {createTime: 1449119707026, text: "准备周三分享"},
+        {createTime: 1449119707026, text: "Q4绩效考核"},
+        {createTime: 1449119707026, text: "消息中心开发"},
+        {createTime: 1449119707026, text: "公共组件交互"}
     ]
 }
