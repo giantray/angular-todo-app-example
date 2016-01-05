d52e84200bd7edf910a5fca10d2cd2d7730b43fd
lizeyang <lizeyang@yy.com>
2016-01-05 18:40:14 +0800

    删除及批量删除

--- pages/todo/todo.ctrl.js
+++ pages/todo/todo.ctrl.js
@@ -9,8 +9,37 @@ angular.module('myApp').controller('TodoCtrl', function ($scope) {
         $scope.list.push({createTime: new Date().getTime(), text: $scope.newTodo});
     }
 
-    $scope.isShowInputTip = function(){
+    $scope.isShowInputTip = function () {
         return $scope.newTodo && $scope.newTodo.length != 0;
+    }
+
+    $scope.removeTodo = function (selectItem) {
+        var index = $scope.list.indexOf(selectItem);
+        $scope.list.splice(index, 1);
+    }
+
+    $scope.hasCompletedItems = function () {
+        return $scope.getCompletedItemSum() > 0;
+    }
+
+    $scope.cleanCompletedItems = function () {
+        var listAfterClean = [];
+        angular.forEach($scope.list, function (item) {
+            if(!item.done){
+                listAfterClean.push(item);
+            }
+        })
+        $scope.list = listAfterClean;
+    }
+
+    $scope.getCompletedItemSum = function () {
+        var sum = 0;
+        angular.forEach($scope.list, function (item) {
+            if(item.done){
+                sum ++;
+            }
+        })
+        return sum;
     }
 
 });
