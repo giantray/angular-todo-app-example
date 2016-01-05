39ba4e389b4719587fab4bc99d59aeb74985cc39
lizeyang <lizeyang@yy.com>
2016-01-05 16:53:13 +0800

    渲染todo列表

--- pages/todo/todo.ctrl.js
+++ pages/todo/todo.ctrl.js
@@ -1,7 +1,17 @@
 angular.module('myApp').controller('TodoCtrl', function($scope){
 
+    $scope.list = getToDoList();
+
+
     //$scope.name = 'lizeyang';
 
-
-
 });
+
+function getToDoList(){
+    return [
+        "准备周三分享",
+        "Q4绩效考核",
+        "消息中心开发",
+        "公共组件交互"
+    ]
+}
