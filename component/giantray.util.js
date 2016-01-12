10233f30fea9539e989b75fd8f983c122f062244
lizeyang <lizeyang@yy.com>
2016-01-12 16:39:16 +0800

    加入依赖注入

--- component/giantray.util.js
+++ component/giantray.util.js
@@ -1,13 +1,13 @@
-angular.module('giantray.util',[]);
+angular.module('giantray.util', []);
 
-angular.module('giantray.util').factory('localStorageSvr',function(){
-   var util = {};
+angular.module('giantray.util').factory('localStorageSvr', function () {
+    var util = {};
 
-    util.getItem = function(key){
-        return window.localStorage? localStorage.getItem(key): Cookie.read(key);
+    util.getItem = function (key) {
+        return window.localStorage ? localStorage.getItem(key) : Cookie.read(key);
     };
 
-    util.setItem = function(key,val){
+    util.setItem = function (key, val) {
         if (window.localStorage) {
             localStorage.setItem(key, val);
         } else {
@@ -18,7 +18,7 @@ angular.module('giantray.util').factory('localStorageSvr',function(){
     return util;
 });
 
-angular.module('giantray.util').filter('prettyDate',function($filter){
+angular.module('giantray.util').filter('prettyDate', ["$filter", function ($filter) {
     return function (time) {
 
         var date = new Date(time || ""),
@@ -32,4 +32,4 @@ angular.module('giantray.util').filter('prettyDate',function($filter){
         return day_diff == 0 && (
             diff < 60 && "刚刚" || diff < 120 && "1 分钟之前" || diff < 3600 && Math.floor(diff / 60) + " 分钟之前" || diff < 7200 && "1 小时之前" || diff < 86400 && Math.floor(diff / 3600) + " 小时之前") || day_diff == 1 && "昨天" || day_diff < 7 && day_diff + " 天之前" || day_diff < 31 && Math.ceil(day_diff / 7) + " 星期之前";
     }
-})
+}]);
